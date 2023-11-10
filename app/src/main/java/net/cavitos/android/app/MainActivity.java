f69978091f30b62edcccc6e412b71dd5d5cca17b
Francisco Castellanos <fvcastellanos@gmail.com>
2023-11-09 21:14:10 -0600

    add first android application

---
+++ app/src/main/java/net/cavitos/android/app/MainActivity.java
@@ -0,0 +1,46 @@
+package net.cavitos.android.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edName = findViewById(R.id.edName);
+    }
+
+    public void showValue(final View view) {
+
+        final var name = edName.getText()
+                        .toString();
+
+        String message = "Ingrese un nombre, por favor";
+
+        if (!StringUtils.isBlank(name)) {
+
+            message = String.format("Bienvenido: %s", name);
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    public void nextLayout(final View view) {
+
+        final var intent = new Intent(this, SecondScreen.class);
+        startActivity(intent);
+    }
+}
